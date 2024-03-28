960a34a4f86bf29e735dc35ce96696cf29c63976
WestonianCrush <westonmc1111@gmail.com>
2024-03-28 12:11:14 -0500

    did printing assignment

--- Unit1/1PrintingAssignment.py
+++ Unit1/1PrintingAssignment.py
@@ -1,21 +1,17 @@
 print("########## 1.1.1 ##########")
-# your code for exercise 1.1.1 goes here
+print("😊")
 
 print("########## 1.1.2 ##########")
-# modify the code below for exercise 1.1.2
-print("Doc")
-print("Grumpy")
-print("Sleepy")
-print("Bashful")
-print("Happy")
-print("Sneezy")
-print("Dopey")
+print("Bashful, Doc, Dopey, Grumpy, Happy, Sleepy, Sneezy")
 
 print("########## 1.1.3 ##########")
-# your code for exercise 1.1.3 goes here
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
 
 print("########## 1.1.4 ##########")
-# your code for exercise 1.1.4 goes here
+print(365*24*60)
 
 print("########## 1.1.5 ##########")
-# your code for exercise 1.1.5 goes here
+print('print("Hello World")')
