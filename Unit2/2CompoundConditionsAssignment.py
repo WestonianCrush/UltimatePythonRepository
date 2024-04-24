768df9012b130a7b83bdb71d993eb6f9be7e72f1
WestonianCrush <westonmc1111@gmail.com>
2024-04-24 12:10:22 -0500

    3.1 and 2.2 complete

--- Unit2/2CompoundConditionsAssignment.py
+++ Unit2/2CompoundConditionsAssignment.py
@@ -1,11 +1,62 @@
 # print("########## 2.2.1 ##########")
-
+# age = int(input("What is your age? "))
+# if age <= 5 and age >= 1:
+#     print("You are too young")
+# elif age < 0:
+#     print("Enter a valid number")
+# else:
+#     print("You are" , age , "years old.")
 # print("########## 2.2.2 ##########")
-
+# name = input("What is your name? ")
+# if name == "Huey" or name == "Dewey" or name == "Louie":
+#     print("You could be one of Donald Ducks nephews")
+# elif name == "Morty" or name == "Ferdie":
+#     print("You could be one of Mickey Mouses nephews")
+# else:
+#     print("You cant be a nephew of a character I know of")
 # print("########## 2.2.3 ##########")
-
+# grade = int(input("Enter your score: "))
+# if grade <= 100 and grade >= 90:
+#     print("You have an A")
+# elif grade <= 89 and grade >= 80:
+#     print("You have a B")
+# elif grade <= 79 and grade >= 70:
+#     print("You have a C")
+# elif grade <= 69 and grade >= 60:
+#     print("You have a D")
+# elif grade <= 59 and grade >= 0:
+#     print("You have an F")
+# elif grade < 0 or grade > 100:
+#     print("Enter a valid percent")
 # print("########## 2.2.4 ##########")
-
+# Number = int(input("Enter a number: "))
+# if Number % 5 == 0 and Number % 3 == 0:
+#     print("BuzzFizz")
+# elif Number % 3 == 0:
+#     print("Fizz")
+# elif Number % 5 == 0:
+#     print("Buzz")
 # print("########## 2.2.5 ##########")
-
+# year = int(input("Type in a year: "))
+# if year % 4 == 0 and year % 100 == 0:
+#     print("That is not a leap year")
+# elif year % 400 == 0:
+#     print("That is a leap year")
+# elif year % 4 == 0:
+#     print("That is a leap year")
+# else:
+#     print("That is not a leap year")
 # print("########## 2.2.6 ##########")
+let1 = input("1st letter: ")
+let2 = input("2nd letter: ")
+let3 = input("3rd letter: ")
+if let1 > let2 and let1 < let3:
+    print(let1)
+elif let2 < let1 and let2 > let3:
+    print(let2)
+elif let2 > let1 and let2 < let3:
+    print(let2)
+elif let1 < let2 and let1 > let3:
+    print(let1)
+else:
+    print(let3)
--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,45 @@
 # ========== 3.1.1 ==========
-
-
+# num = 2
+# while num < 31:
+#     print(num)
+#     num += 2
 # ========== 3.1.2 ==========
 
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number > 0:
+#     print(number)
+#     number -= 1
 # print("Go!")
 
 # ========== 3.1.3 ==========
-
+# numb = 1
+# limit = int(input("Upper limit: "))
+# while limit > numb:
+#     print(numb)
+#     numb += 1
 
 # ========== 3.1.4 ==========
+# num1 = 1
+# limit1 = int(input("Upper limit: "))
+# while limit1 + 1 > num1:
+#     print(num1)
+#     num1 *= 2
 
 
 # ========== 3.1.5 ==========
-
+# start = 1
+# lim = int(input("Upper Limit: "))
+# base = int(input("Base: "))
+# while lim + 1 > start:
+#     print(start)
+#     start *= base
 
 # ========== 3.1.6 ==========
+numm = 0
+numm2 = 1
+numlimit = int(input("Limit: "))
+while numlimit > numm:
+    numm += numm2
+    numm2 += 1
+print(numm)
