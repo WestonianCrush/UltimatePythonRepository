15bbe7a998d04be578837d94972dc86cb951144d
WestonianCrush <westonmc1111@gmail.com>
2024-05-03 11:29:59 -0500

    3.3 done and half of 3.2

--- Unit3/2BasicStringAssignment.py
+++ Unit3/2BasicStringAssignment.py
@@ -1,20 +1,38 @@
 # ========== 3.2.1 ==========
-
+# word = input("Please type in a string: ")
+# amount = int(input("Please type in an amount: "))
+# print(word*amount)
 
 # ========== 3.2.2 ==========
-
-
+# word1 = input("Please type in string 1: ")
+# word2 = input("Please type in string 2: ")
+# if len(word1) > len(word2):
+#     print(word1 , "is longer")
+# elif len(word1) < len(word2):
+#     print(word2 , "is longer")
+# else:
+#     print("They are equally as long")
 # ========== 3.2.3 ==========
-
+# word = input("please type in a string: ")
+# wordlen = len(word)
+# ex = -1
+# while ex >= -wordlen:
+#         print(word[ex])
+#         ex = ex - 1
 
 # ========== 3.2.4 ==========
 
 
 # ========== 3.2.5 ==========
-
+# width = int(input("Width: "))
+# print("#"*width)
 
 # ========== 3.2.6 ==========
-
+width = int(input("Width: "))
+Height = int(input("Height: "))
+while Height > 0:
+    print("#"*width)
+    Height -= 1
 
 # ========== 3.2.7 ==========
 
--- Unit3/3SlicesAndSearchingAssignment.py
+++ Unit3/3SlicesAndSearchingAssignment.py
@@ -1,17 +1,68 @@
 # ========== 3.3.1 ==========
-
+# text = input("Please type in a string: ")
+# endpos = 1
+# while True:
+#     ex = text[0:endpos]
+#     endpos += 1
+#     print(ex)
+#     if len(ex) == len(text):
+#         break
 
 # ========== 3.3.2 ==========
-
-
+# text = input("Please type in a string: ")
+# endpos = len(text) - 1
+# while True:
+#     ex = text[endpos:len(text)]
+#     endpos -= 1
+#     print (ex)
+#     if len(ex) == len(text):
+#         break
 # ========== 3.3.3 ==========
-
-
+# text = input("Please type in a string: ")
+# if "a" in text:
+#     print("A found")
+# else:
+#     print("A not found")
+# if "e" in text:
+#     print("E found")
+# else:
+#     print("E not found")
+# if "o" in text:
+#     print("O found")
+# else:
+#     print("O not found")
 # ========== 3.3.4 ==========
-
-
+# word = input("Please type in a word: ")
+# char = input("Please type in a character: ")
+# if char in word:
+#     ex = word.find(char)
+#     if ex+3 > len(word):
+#         print()
+#     else:
+#         print(word[ex:ex+3])
+        
 # ========== 3.3.5 ==========
-
+# word = input("Please type in a word: ")
+# char = input("Please type in a character: ")
+# while True:
+#     if char in word:
+#         ex = word.find(char)
+#     elif ex+3 > len(word):
+#         print()
+#     else:
+#         print(word)
+#         word = word[2:]
 
 # ========== 3.3.6 ==========
+string = input("Please type in a string: ")
+sub = input("Please type in a substring: ")
+position = string.find(sub)
+part = string[position+1:]
+newpos = part.find(sub)
+finalpos = position + newpos + 1
+if newpos == -1:
+        print("This Substring does not occur twice")
+else:
+        print("The 2nd occurrence of the substring is at index:" , finalpos )
+ 
 
