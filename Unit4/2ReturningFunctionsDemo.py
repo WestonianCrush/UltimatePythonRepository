efe3eb1b702741dd04ad4c0bc46cf35a36eafa16
WestonianCrush <westonmc1111@gmail.com>
2024-05-16 12:32:24 -0500

    4.3 and 4.2 done

--- Unit4/2ReturningFunctionsDemo.py
+++ Unit4/2ReturningFunctionsDemo.py
@@ -10,13 +10,27 @@
 # print(greatest(0, 0, 0))
 
 # ========== 4.2.2 ==========
-def same(word, index1, index2):
-    if word[index1:] == word[index2:]:
-        return True
-print(same("programmer", 6, 7))
-
+# def same(word, index1, index2):
+#     if index2 > len(word):
+#         return False
+#     elif word[index1] == word[index2]:
+#         return True
+#     elif word[index1] != word[index2]:
+#         return False
+    
+# print(same("programmer", 0, 4))
+# print(same("programmer", 6, 7))
+# print(same("programmer", 0, 12))
 
 # ========== 4.2.3 ==========
+sentence = "My name is Sam"
+def first(first):
+    return sentence[0:2]
+def second(second):
+    return sentence[3:6]
+def final(final):
+    return sentence[25:]
 
-
-
+print(first(sentence))
+print(second(sentence))
+print(final(sentence))
--- Unit4/3ListsIntroAssignment.py
+++ Unit4/3ListsIntroAssignment.py
@@ -1,12 +1,43 @@
 # ========== 4.3.1 ==========
-
-
+# list = [1, 2, 3, 4, 5]
+# while True:
+#     index = int(input("Index: "))
+#     if index == -1:
+#         break
+#     New = int(input("New Value: "))
+#     list.pop(index)
+#     list.insert(index, New)
+#     print(list)
 # ========== 4.3.2 ==========
-
-
+# list = []
+# items = int(input("How many items: "))
+# while items > 0:
+#     word = input(f"Item {items}: ")
+#     list.append(word)
+#     items -= 1
+# print(list)
 # ========== 4.3.3 ==========
-
-
+# list = []
+# while True:
+#     print("The list is now", list)
+#     option = input("A(d)d, (r)emove or e(x)it: ")
+#     if option == "x":
+#         print("Bye!")
+#         break
+#     elif option == "d":
+#         list.append(len(list)+1)
+#     elif option == "r":
+#         list.remove(len(list))
 # ========== 4.3.4 ==========
-
+list = []
+num = 0
+while True:
+     word = input("Word: ")
+     if word in list:
+        print("You typed in", num, "different words")
+        break
+     else: 
+        list.append(word)
+        num += 1
+        print(list)
 
