9b13de24134ed844a36b57976a9b654456bcfd51
WestonianCrush <westonmc1111@gmail.com>
2024-05-20 12:55:21 -0500

    4.4 more finished

--- Unit4/4ForLoopsAssignment.py
+++ Unit4/4ForLoopsAssignment.py
@@ -7,19 +7,26 @@
 #     index += 1
 
 # ========== 4.4.2 ==========
-num = int(input("Please type in a positive integer: "))
-newnum = num * -1
-while num > newnum:
-    print(newnum)
-    newnum += 1
-    if newnum == 0:
-        newnum += 1
-        print(newnum)
+# num = int(input("Please type in a positive integer: "))
+# newnum = num * -1
+# while num > newnum:
+#     print(newnum)
+#     newnum += 1
+#     if newnum == 0:
+#         newnum += 1
+#         print(newnum)
 
 
 
 # ========== 4.4.3 ==========
+def stars(num, num2, num3, num4, num5):
+    print("*"*num)
+    print("*"*num2)
+    print("*"*num3)
+    print("*"*num4)
+    print("*"*num5)
 
+stars(3, 7, 1, 1, 2)
 
 # ========== 4.4.4 ==========
 
