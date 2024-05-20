09c5b8705dd75d2b7f9324df05b3cc86fad62c52
WestonianCrush <westonmc1111@gmail.com>
2024-05-20 11:56:38 -0500

    4.4 half done

--- Unit4/4ForLoopsAssignment.py
+++ Unit4/4ForLoopsAssignment.py
@@ -1,7 +1,21 @@
 # ========== 4.4.1 ==========
-
+# index = 0
+# word = input("Please type in a string: ")
+# while index < len(word):
+#     print(word[index:index+1])
+#     print("*")
+#     index += 1
 
 # ========== 4.4.2 ==========
+num = int(input("Please type in a positive integer: "))
+newnum = num * -1
+while num > newnum:
+    print(newnum)
+    newnum += 1
+    if newnum == 0:
+        newnum += 1
+        print(newnum)
+
 
 
 # ========== 4.4.3 ==========
